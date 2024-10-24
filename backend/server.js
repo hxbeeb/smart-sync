6d7ec8402bf8607356cf1d3c2c802b40584a5375
GLORIENZE <habeebsalehalhussain@gmail.com>
2024-10-24 15:40:19 +0530

    k

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const { useParams } = require('react-router-dom');
 const app = express();
 const bcrypt=require('bcrypt');
 const jwt = require('jsonwebtoken');
