c322d73a4e7df2dc4eb3843cb736f51d082e0fb0
GLORIENZE <habeebsalehalhussain@gmail.com>
2024-10-27 07:13:32 +0530

    final

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -82,26 +82,26 @@ const Dashboard = () => {
           {/* Projects Display */}
           {projects.length > 0 ? (
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-              {projects.map((project) => (
-                <Link
-                  key={project._id}
-                  to={`/projects/${project._id}`}
-                  className="bg-gradient-to-r from-green-400 to-blue-500 text-white shadow-lg rounded-lg p-6 transition-transform transform hover:scale-105 hover:shadow-xl hover:bg-gradient-to-l hover:from-blue-500 hover:to-green-400"
-                >
-                  <h2 className="text-xl font-bold mb-2">{project.name}</h2>
-                  <p className="text-sm">Deadline: {project.end_date}</p>
-                  <div className="relative pt-4">
-                    <div className="h-2 bg-gray-300 rounded-full">
-                      <div
-                        className="h-2 bg-blue-600 rounded-full"
-                        style={{ width: `${project.progress}%` }}
-                      ></div>
-                    </div>
-                    <p className="text-right text-sm mt-1">{project.progress}% Complete</p>
+            {projects.map((project) => (
+              <div
+                key={project._id}
+                onClick={() => navigate(`/projectss/${selectedDepartment.id}/${project._id}`, { state: { name: project.name, progress: project.progress } })}
+                className="bg-gradient-to-r from-green-400 to-blue-500 text-white shadow-lg rounded-lg p-6 transition-transform transform hover:scale-105 hover:shadow-xl hover:bg-gradient-to-l hover:from-blue-500 hover:to-green-400 cursor-pointer"
+              >
+                <h2 className="text-xl font-bold mb-2">{project.name}</h2>
+                <div className="relative pt-4">
+                  <div className="h-2 bg-gray-300 rounded-full">
+                    <div
+                      className="h-2 bg-blue-600 rounded-full"
+                      style={{ width: `${project.progress}%` }}
+                    ></div>
                   </div>
-                </Link>
-              ))}
-            </div>
+                  <p className="text-right text-sm mt-1">{project.progress}% Complete</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          
           ) : (
             <p className="text-center text-gray-500">No projects available at the moment.</p>
           )}
